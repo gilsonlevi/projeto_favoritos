bd47dbf5cabfead6f2978463f174a4e26b947af0
Gilson Levi <117370090+gilsonlevi@users.noreply.github.com>
2023-09-15 11:45:55 -0300

    Commit initial

---
+++ src/componentes/Conteudo/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Conteudo.module.css'
+//Esse componente exporta uma função
+//Uma função representa o conteúdo
+export default function Conteudo(props) {
+  return (
+    <>
+      <div className={styles.conteudo}>
+        <article>
+          <img src={props.img} alt="Imagem"/>
+          <p>NESCAFÉ DOLCE GUSTO Mocha 10 cápsulas
+            R$ 20,90
+            R$ 17,97 no Pix</p>
+        </article>
+      </div>
+    </>
+  )
+}
+
+export function OlaMundo(){
+    console.log("Olá mundo")
+}
+
